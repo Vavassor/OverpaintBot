77f3000c5cfa401657e86fdd9baf6c6cb170bcdf
Vavassor <copernicus-@hotmail.com>
2017-01-20 01:18:40 -0500

    =Dedicated to the public domain more explicitly.

--- overpaint_bot.py
+++ overpaint_bot.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python3
 # coding: utf-8
+#
+# OverpaintBot - an art bot that posts to the social network server Mastodon
+# 
+# Written in 2016 by Andrew Dawson
+#
+# To the extent possible under law, the author(s) have dedicated all copyright
+# and related and neighboring rights to this software to the public domain
+# worldwide. This software is distributed without any warranty.
+#
+# You should have received a copy of the CC0 Public Domain Dedication along
+# with this software. If not, see
+# <http://creativecommons.org/publicdomain/zero/1.0/>.
 
 import sys
 import argparse
